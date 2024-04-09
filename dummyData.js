6314112204ef879fa6ed7232109f2ad0f9275fea
Rondarik <89934006+Rondarik@users.noreply.github.com>
2024-04-09 14:16:12 +0200

    push for merge in Board

--- dummyData.js
+++ dummyData.js
@@ -1,5 +1,6 @@
 let dummyTasks = [
     {
+        "taskID": "0",
         "title": "Task 1",
         "description": "Description for Task 1",
         "assignedTo": [],
@@ -9,6 +10,7 @@ let dummyTasks = [
         "subtasks": []
     },
     {
+        "taskID": "1",
         "title": "Task 2",
         "description": "Description for Task 2",
         "assignedTo": [],
@@ -18,6 +20,7 @@ let dummyTasks = [
         "subtasks": []
     },
     {
+        "taskID": "2",
         "title": "Task 3",
         "description": "Description for Task 3",
         "assignedTo": [],
@@ -27,6 +30,7 @@ let dummyTasks = [
         "subtasks": []
     },
     {
+        "taskID": "3",
         "title": "Task 4",
         "description": "Description for Task 4",
         "assignedTo": [],
@@ -35,7 +39,8 @@ let dummyTasks = [
         "category": "user story",
         "subtasks": []
     },
-    {
+    {   
+        "taskID": "4",
         "title": "Task 5",
         "description": "Description for Task 5",
         "assignedTo": [],
@@ -53,31 +58,41 @@ let dummyContacts = [
     {
         "name": "Max Mustermann",
         "eMail": "max.mustermann@example.com",
+        "password":"pw11",
         "tel": "+1234567890",
-        "color": "#acb123"
+        "color": "#acb123",
+        "Tasks": []
     },
     {
         "name": "Maria Müller",
         "eMail": "maria.mueller@example.com",
+        "password":"pw22",
         "tel": "+1987654321",
-        "color": "#456def"
+        "color": "#456def",
+        "Tasks": []
     },
     {
         "name": "John Doe",
         "eMail": "john.doe@example.com",
+        "password":"pw33",
         "tel": "+1122334455",
-        "color": "#789abc"
+        "color": "#789abc",
+        "Tasks": []
     },
     {
         "name": "Jane Smith",
         "eMail": "jane.smith@example.com",
+        "password":"pw44",
         "tel": "+1555666777",
-        "color": "#f0a1b2"
+        "color": "#f0a1b2",
+        "Tasks": []
     },
     {
         "name": "Anna Schmidt",
         "eMail": "anna.schmidt@example.com",
+        "password":"pw55",
         "tel": "+1444333222",
-        "color": "#fedcba"
+        "color": "#fedcba",
+        "Tasks": []
     }
 ];
