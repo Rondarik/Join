def346b88cc05034a35da43146dccf6eac6ca5fe
Rondarik <89934006+Rondarik@users.noreply.github.com>
2024-04-10 13:47:06 +0200

    update serverCommunication.js

--- serverCommunication.js
+++ serverCommunication.js
@@ -1,4 +1,4 @@
-const STORAGE_TOKEN = GMRE7FNSDEXMO4KXUOG94GLBJAO0BM9MB1CKCLIB;
+const STORAGE_TOKEN = 'GMRE7FNSDEXMO4KXUOG94GLBJAO0BM9MB1CKCLIB';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 async function setItem(key, value) {
